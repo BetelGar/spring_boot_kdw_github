0fefac4b3eaaa64f50fd1ddc1e860658b91091c1
DESKTOP-D4P20FS\skype <sprkahxhalcl@naver.com>
2022-07-11 10:06:32 +0900

    first commit

---
+++ src/main/java/edu/hi/ex/SpringBootKdwGithubApplication.java
@@ -0,0 +1,13 @@
+package edu.hi.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootKdwGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootKdwGithubApplication.class, args);
+	}
+
+}
