bdcbf95a7fecbff25598085a1b33c7bb0581f51c
DESKTOP-D4P20FS\skype <sprkahxhalcl@naver.com>
2022-07-11 10:32:18 +0900

    second commit

--- src/main/java/edu/hi/ex/SpringBootKdwGithubApplication.java
+++ src/main/java/edu/hi/ex/SpringBootKdwGithubApplication.java
@@ -2,10 +2,17 @@ package edu.hi.ex;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
+
+
+//github 테스트
 @SpringBootApplication
 public class SpringBootKdwGithubApplication {
 
+	
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootKdwGithubApplication.class, args);
 	}
