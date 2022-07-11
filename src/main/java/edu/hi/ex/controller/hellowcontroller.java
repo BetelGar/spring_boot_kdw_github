464426e07cf9a9834dbc95bf81e8eb053225ef67
DESKTOP-D4P20FS\skype <sprkahxhalcl@naver.com>
2022-07-11 10:38:51 +0900

    third

---
+++ src/main/java/edu/hi/ex/controller/hellowcontroller.java
@@ -0,0 +1,13 @@
+package edu.hi.ex.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class hellowcontroller {
+
+	@GetMapping("/")
+	public String Home() {
+		return "Hello World";
+	}
+}
